6049039404729434a320ffdeca0dbd29ad42b842
Jennifer Birsan <jennibirsan@Cornels-MacBook-Pro.local>
2019-07-03 12:32:19 -0500

    simplified liri.js file and added bands in town

--- keys.js
+++ keys.js
@@ -9,14 +9,17 @@
 var axios = require("axios");
 var fs = require("fs");
 
-var KEYS = function() {
+var KEYS = function () {
 
   var divider = "\n------------------------------------------------------------\n\n";
+
+  // SHOW SEARCH
+
   // findShow takes in the name of a tv show and searches the tvmaze API
-  this.findShow = function(show) {
+  this.findShow = function (show) {
     var URL = "http://api.tvmaze.com/singlesearch/shows?q=" + show;
 
-    axios.get(URL).then(function(response) {
+    axios.get(URL).then(function (response) {
       // Place the response.data into a variable, jsonData.
       var jsonData = response.data;
 
@@ -30,16 +33,18 @@ var KEYS = function() {
       ].join("\n\n");
 
       // Append showData and the divider to log.txt, print showData to the console
-      fs.appendFile("log.txt", showData + divider, function(err) {
+      fs.appendFile("log.txt", showData + divider, function (err) {
         if (err) throw err;
         console.log(showData);
       });
     });
   };
 
-  this.findActor = function(actor) {
+  // ACTOR SEARCH
+
+  this.findActor = function (actor) {
     var URL = "http://api.tvmaze.com/search/people?q=" + actor;
-    axios.get(URL).then(function(response) {
+    axios.get(URL).then(function (response) {
       // Place the response.data into a variable, jsonData.
       var jsonData = response.data;
 
@@ -54,39 +59,63 @@ var KEYS = function() {
       ].join("\n\n");
 
       // Append actorData and the divider to log.txt, print actorData to the console
-      fs.appendFile("log.txt", actorData + divider, function(err) {
+      fs.appendFile("log.txt", actorData + divider, function (err) {
         if (err) throw err;
         console.log(actorData);
       });
     });
-    };
+  };
 
-    // MOVIE
+  // MOVIE SEARCH
 
-    this.findMovie = function(movie) {
-      var URL = "http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy"
-      axios.get(URL).then(function(response) {
-        // Place the response.data into a variable, jsonData.
-        var jsonData = response.data;
-  
-        // movieData ends up being the string containing the movie data we will print to the console
-        var movieData = [
-          "Movie Title: " + jsonData.Title,
-         "Release Year: " + jsonData.Year,
-          "IMBD Rating: " + jsonData.imdbRating,
-         "Country: " + jsonData.Country,
-          "Language: " + jsonData.Language,
-       "Plot: " + jsonData.Plot,
+  this.findMovie = function (movie) {
+    var URL = "http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy"
+    axios.get(URL).then(function (response) {
+      // Place the response.data into a variable, jsonData.
+      var jsonData = response.data;
+
+      // movieData ends up being the string containing the movie data we will print to the console
+      var movieData = [
+        "Movie Title: " + jsonData.Title,
+        "Release Year: " + jsonData.Year,
+        "IMBD Rating: " + jsonData.imdbRating,
+        "Country: " + jsonData.Country,
+        "Language: " + jsonData.Language,
+        "Plot: " + jsonData.Plot,
         "Actors: " + jsonData.Actors,
-        ].join("\n\n");
-  
-        // Append movieData and the divider to log.txt, print movieData to the console
-        fs.appendFile("log.txt", movieData + divider, function(err) {
-          if (err) throw err;
-          console.log(movieData);
-        });
+      ].join("\n\n");
+
+      // Append movieData and the divider to log.txt, print movieData to the console
+      fs.appendFile("log.txt", movieData + divider, function (err) {
+        if (err) throw err;
+        console.log(movieData);
       });
-      };
+    });
+  };
+
+  // CONCERT SEARCH
+  this.findConcert = function (artist) {
+    var URL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"
+    axios.get(URL).then(function (response) {
+      // Place the response.data into a variable, jsonData.
+      var jsonData = response.data;
+
+      // concertData ends up being the string containing the concert data we will print to the console
+      var concertData = [
+        "Name of Venue: " + jsonData.Venue,
+        "Location of Venue: " + jsonData.Location,
+        "Date of Event: " + jsonData.Date,
+      ].join("\n\n");
+
+      // Append concertData and the divider to log.txt, print concertData to the console
+      fs.appendFile("log.txt", concertData + divider, function (err) {
+        if (err) throw err;
+        console.log(concertData);
+      });
+    });
+  };
+
+
 };
 
 
--- liri.js
+++ liri.js
@@ -3,31 +3,38 @@ require("dotenv").config();
 var KEYS = require("./keys.js");
 // var spotify = new Spotify(keys.spotify);
 
-// Create a new TV object
+// Create a new keys object
 var keys = new KEYS();
 
 // Grab search command line argument
-var search = process.argv[2];
-var term = process.argv.slice(3).join(" ");
+var command = process.argv[2].toLowerCase();
+var userSearch = process.argv.slice(3).join(" ");
 
-if (!search) {
-  search = "movie";
+// Switch command for user when searching for a show, actor, movie, song, or concert.
+
+function switchCommand() {
+  switch (command) {
+    case "show":
+      console.log("User is searching for a", command, "called", userSearch);
+      keys.findShow(userSearch);
+      break;
+    case "actor":
+      console.log("User is searching for an", command, "by the name of", userSearch);
+      keys.findActor(userSearch);
+      break;
+    case "movie":
+      console.log("User is searching for a", command, "by the name of", userSearch);
+      keys.findMovie(userSearch);
+      break;
+    case "song":
+      console.log("User is searching for a", command, "by the name of", userSearch);
+      keys.findSong(userSearch);
+      break;
+    case "artist":
+      console.log("User is searching for a", command, "by the name of", userSearch);
+      keys.findConcert(userSearch);
+      break;
+
+  }
 }
-
-if (!term) {
-  term = "Prisoners";
-}
-
-// Print whether searching for a show or actor, print the term as well
-if (search === "show") {
-  console.log("Searching for TV Show");
-  keys.findShow(term);
-} else if (search === "actor"){
-    keys.findActor(term);
-  console.log("Searching for TV Actor");
-} else if (search === "movie"){
-        keys.findMovie(term);
-        console.log("Searching for Movie");
-    
-}
-
+switchCommand();
